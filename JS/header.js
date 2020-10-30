b956baf498679f430415c9c95f786c5ca0ed4f17
sidatube <70898455+sidatube@users.noreply.github.com>
2020-10-30 15:41:22 +0700

    Mau

--- JS/header.js
+++ JS/header.js
@@ -1,8 +1,9 @@
 function header(){
-    return " <div class=\"container\">\n" +
+    return "<div class=\"nendo\">\n" +
+        "        <div class=\"container\">\n" +
         "            <div class=\"floatleft\">\n" +
         "                <ul class=\"floatleft ulngang\">\n" +
-        "                    <li class=\"xuong\"> ĐC: 70B An Dương - Yên Phụ - Tây Hồ</li>\n" +
+        "                    <li class=\"xuong\">ĐC: 70B An Dương - Yên Phụ - Tây Hồ</li>\n" +
         "                    <li><a class=\"linkkomau xuong\" href=\"tel:0902040990\">Hotline: 090.2040.990</a></li>\n" +
         "                    <li><strong class=\"uppercase\"> <a class=\"linktrang\" href=\"Home.html\"> Bánh sinh nhật tokyo gâteaux </a>|</strong></li>\n" +
         "                </ul>\n" +
@@ -13,7 +14,30 @@ function header(){
         "                <div class=\"floatright vuong tron fb mautrang denmail\"><a class=\"linkkomau\" href=\"mailto:tokyogateaux@gmail.com\"><i class=\" far fa-envelope\"></i></a></div>\n" +
         "                <div class=\"floatright vuong tron fb mautrang xanhins\"><a class=\"linkkomau\" href=\"https://www.instagram.com/tokyo.gateaux/\"><i class=\" fab fa-instagram\"></i></a></div>\n" +
         "                <div class=\"floatright vuong mautrang xanhfb tron fb\"><a class=\"linkkomau\" href=\"https://www.facebook.com/tiembanhsinhnhattokyogateaux/\"><i class=\" fab fa-facebook-f\"></i></a></div>\n" +
-        "            </div>\n"
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "    <div class=\"nentrang\">\n" +
+        "        <div class=\"container\">\n" +
+        "            <div class=\"top-left-2\">\n" +
+        "                <ul>\n" +
+        "                    <li class=\"top-left-2-0\"><a href=\"https://tokyogateaux.vn/\"><img src=\"imgs/Untitled-7.jpg\" height=\"48\" width=\"225\"/></a></li>\n" +
+        "                    <li class=\"top-left-2-1\"><a class=\"top-left-2-2\" href=\"#\"><i class=\"fas fa-search\"></i></a></li>\n" +
+        "                    <li class=\"top-left-2-1\"><a class=\"top-left-2-2\" href=\"https://tokyogateaux.vn/\">TRANG CHỦ</a></li>\n" +
+        "                    <li class=\"top-left-2-1\"><a class=\"top-left-2-2\" href=\"\">BÁNH SINH NHẬT</a></li>\n" +
+        "                    <li class=\"top-left-2-1\"><a class=\"top-left-2-2\" href=\"\">BLOG</a></li>\n" +
+        "                    <li class=\"top-left-2-1\"><a class=\"top-left-2-2\" href=\"\">GIỎ HÀNG</a></li>\n" +
+        "                </ul>\n" +
+        "            </div>\n" +
+        "            <div class=\"top-right-2\">\n" +
+        "                <ul>\n" +
+        "                    <li><a class=\"top-right-2-1\" href=\"https://tokyogateaux.vn/cart/\"><i class=\"fas fa-shopping-cart\"></i></a></li>\n" +
+        "                    <li class=\"top-right-2-2\"><a class=\"top-right-2-3\" href=\"#\">CART / 0đ</a></li>\n" +
+        "                </ul>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "\n" +
+        "    </div>"
 }
 var hd = document.getElementById("header");
 hd.innerHTML = header();
