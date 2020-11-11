680a5fe4ccaa1e7626d262e0d100d8897c0e487e
truongquangdang97 <70947896+truongquangdang97@users.noreply.github.com>
2020-11-11 17:03:32 +0700

    quang

---
+++ JS/phanchinh.js
@@ -0,0 +1,168 @@
+function phanchinh(){
+    return"                <div class=\"col-lg-9\">\n" +
+        "                    <div class=\"S\">\n" +
+        "                        <div class=\"Y\">\n" +
+        "                            <h1>Bánh sinh nhật hiện </h1>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-lg-4\">\n" +
+        "                            <div class=\"TT\">\n" +
+        "                                <div class=\"T\">\n" +
+        "                                    <img src=\"photo/Ảnh chụp Màn hình 2020-10-28 lúc 16.06.34.png\">\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T9\"><i class=\"fas fa-heart\"></i></div>\n" +
+        "                                <div class=\"T1\">\n" +
+        "                                    <div class=\"T2\">\n" +
+        "                                        Bánh sinh nhật\n" +
+        "                                    </div>\n" +
+        "                                    <div class=\"T3\">\n" +
+        "                                        <p class=\"height40px\"><a class=\"T4\" href=\"#\">\n" +
+        "                                            Bánh sinh nhật kem sữa chua mix dâu tây tươi sz16\n" +
+        "                                        </a></p>\n" +
+        "                                    </div>\n" +
+        "                                    <div class=\"T5\">\n" +
+        "                                        <b>290.000đ</b>\n" +
+        "                                    </div>\n" +
+        "                                    <div class=\"T6\">\n" +
+        "                                        <a class=\"T7\" href=\"#\">ADD  TO CART</a>\n" +
+        "                                    </div>\n" +
+        "                                </div>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-lg-4\">\n" +
+        "                            <div class=\"TT\">\n" +
+        "                                <div class=\"T\">\n" +
+        "                                    <img src=\"photo/Ảnh chụp Màn hình 2020-10-28 lúc 16.06.51.png\">\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T9\"><i class=\"fas fa-heart\"></i></div>\n" +
+        "                                <div class=\"T2\">\n" +
+        "                                    Bánh sinh nhật\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T3\">\n" +
+        "                                    <p class=\"height40px\"><a class=\"T4\" href=\"#\">\n" +
+        "                                        Bánh sinh nhật phủ socola sz18\n" +
+        "                                    </a></p>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T5\">\n" +
+        "                                    <b>270.000đ</b>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T6\">\n" +
+        "                                    <a class=\"T7\" href=\"#\">ADD  TO CART</a>\n" +
+        "                                </div>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-lg-4\">\n" +
+        "                            <div class=\"TT\">\n" +
+        "                                <div class=\"T\">\n" +
+        "                                    <img src=\"photo/Ảnh chụp Màn hình 2020-10-28 lúc 16.07.05.png\">\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T9\"><i class=\"fas fa-heart\"></i></div>\n" +
+        "                                <div class=\"T2\">\n" +
+        "                                    Bánh sinh nhật\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T3\">\n" +
+        "                                    <p class=\"height40px\"><a class=\"T4\" href=\"#\">\n" +
+        "                                        Bánh sinh nhật đại gia ôtô trắng quý phái sz16\n" +
+        "                                    </a></p>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T5\">\n" +
+        "                                    <b>250.000đ</b>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T6\">\n" +
+        "                                    <a class=\"T7\" href=\"#\">ADD  TO CART</a>\n" +
+        "                                </div>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-lg-4\">\n" +
+        "                            <div class=\"TT\">\n" +
+        "                                <div class=\"T\">\n" +
+        "                                    <img src=\"photo/Ảnh chụp Màn hình 2020-10-28 lúc 16.07.05.png\">\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T9\"><i class=\"fas fa-heart\"></i></div>\n" +
+        "                                <div class=\"T2\">\n" +
+        "                                    Bánh sinh nhật\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T3\">\n" +
+        "                                    <p class=\"height40px\"><a class=\"T4\" href=\"#\">\n" +
+        "                                        Bánh sinh nhật đại gia ôtô trắng quý phái sz1\n" +
+        "                                    </a></p>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T5\">\n" +
+        "                                    <b>250.000đ</b>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T6\">\n" +
+        "                                    <a class=\"T7\" href=\"#\">ADD  TO CART</a>\n" +
+        "                                </div>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-lg-4\">\n" +
+        "                            <div class=\"TT\">\n" +
+        "                                <div class=\"T\">\n" +
+        "                                    <img src=\"photo/Ảnh chụp Màn hình 2020-10-28 lúc 16.06.34.png\">\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T9\"><i class=\"fas fa-heart\"></i></div>\n" +
+        "                                <div class=\"T2\">\n" +
+        "                                    Bánh sinh nhật\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T3\">\n" +
+        "                                    <p class=\"height40px\"><a class=\"T4\" href=\"#\">\n" +
+        "                                        Bánh sinh nhật phủ socola sz18\n" +
+        "                                        cực kì ngon\n" +
+        "                                    </a></p>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T5\">\n" +
+        "                                    <b>270.000đ</b>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T6\">\n" +
+        "                                    <a class=\"T7\" href=\"#\">ADD  TO CART</a>\n" +
+        "                                </div>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"col-lg-4\">\n" +
+        "                            <div class=\"TT\">\n" +
+        "                                <div class=\"T\">\n" +
+        "                                    <img src=\"photo/Ảnh chụp Màn hình 2020-10-28 lúc 16.06.51.png\">\n" +
+        "                                </div>\n" +
+        "                                <div class=\"T9\"><i class=\"fas fa-heart\"></i></div>\n" +
+        "                                <div class=\"T1\">\n" +
+        "                                    <div class=\"T2\">\n" +
+        "                                        Bánh sinh nhật\n" +
+        "                                    </div>\n" +
+        "                                    <div class=\"T3\">\n" +
+        "                                        <p class=\"height40px\"><a class=\"T4\" href=\"#\">\n" +
+        "                                            Bánh sinh nhật kem sữa chua mix dâu tây tươi sz16\n" +
+        "                                        </a></p>\n" +
+        "                                    </div>\n" +
+        "                                    <div class=\"T5\">\n" +
+        "                                        <b>290.000đ</b>\n" +
+        "                                    </div>\n" +
+        "                                    <div class=\"T6\">\n" +
+        "                                        <a class=\"T7\" href=\"#\">ADD  TO CART</a>\n" +
+        "                                    </div>\n" +
+        "                                </div>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"X col-md-12\">\n" +
+        "                        <div class=\"col-md-4\"></div>\n" +
+        "                        <ul class=\"col-md-4\" >\n" +
+        "                            <li class=\"z1\">\n" +
+        "                                <a class=\"z2\" href=\"#\">1</a>\n" +
+        "                            </li>\n" +
+        "                            <li class=\"z1\">\n" +
+        "                                <a class=\"z2\" href=\"#\">2</a>\n" +
+        "                            </li>\n" +
+        "                            <li class=\"z1\">\n" +
+        "                                <a class=\"z2\" href=\"#\">3</a>\n" +
+        "                            </li>\n" +
+        "                            <li class=\"z1\">\n" +
+        "                                <a class=\"z2\" href=\"#\">4</a>\n" +
+        "                            </li>\n" +
+        "                            <li class=\"z1\">\n" +
+        "                                <a class=\"z2\" href=\"#\"><i class=\"fas fa-angle-right\"></i></a>\n" +
+        "                            </li>\n" +
+        "                        </ul>\n" +
+        "                    </div>\n" +
+        "                </div>\n"
+}
+var hd = document.getElementById("phanchinh");
+hd.innerHTML = phanchinh();
