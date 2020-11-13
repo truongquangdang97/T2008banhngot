9aa8d4e8c5dbca55832c53b2004d8612dd4d463d
sidatube <70898455+sidatube@users.noreply.github.com>
2020-11-13 10:51:38 +0700

    Mau

--- JS/header.js
+++ JS/header.js
@@ -1,22 +1,26 @@
 function header(){
     return "<div class=\"nendo\">\n" +
-        "        <div class=\"container\">\n" +
-        "            <div class=\"floatleft\">\n" +
-        "                <ul class=\"floatleft ulngang\">\n" +
-        "                    <li class=\"xuong\">ĐC: 70B An Dương - Yên Phụ - Tây Hồ</li>\n" +
-        "                    <li><a class=\"linkkomau xuong\" href=\"tel:0902040990\">Hotline: 090.2040.990</a></li>\n" +
-        "                    <li><strong class=\"uppercase\"> <a class=\"linktrang\" href=\"Home.html\"> Bánh sinh nhật tokyo gâteaux </a>|</strong></li>\n" +
-        "                </ul>\n" +
+        "        <div class=\"header-1\">\n" +
+        "            <div class=\"container\">\n" +
+        "                <div class=\"top-left-1\">\n" +
+        "                    <ul>\n" +
+        "                        <li class=\"top-left-1-1\">\n" +
+        "                            <a class=\"linktrang\" href=\"#\"> <b>BÁNH SINH NHẬT TOKYO GÂTEAUX</b></a></li>\n" +
+        "                        <li class=\"top-left-1-2\">Hotline: 090.2040.990</li>\n" +
+        "                        <li class=\"top-left-1-3\">ĐC: 70B An Dương - Yên Phụ - Tây Hồ</li>\n" +
+        "                    </ul>\n" +
+        "                </div>\n" +
+        "                <div class=\"top-right-1\">\n" +
+        "                    <ul>\n" +
+        "                        <li><a href=\"https://www.facebook.com/tiembanhsinhnhattokyogateaux/\"><i class=\"fab fa-facebook-f top-right-1-1\"></i></a></li>\n" +
+        "                        <li><a href=\"https://www.instagram.com/tokyo.gateaux/\"><i class=\"fab fa-instagram top-right-1-2\"></i></a></li>\n" +
+        "                        <li><a href=\"mailto:tokyogateaux@gmail.com\"><i class=\"fas fa-envelope top-right-1-3\"></i></a></li>\n" +
+        "                        <li><a href=\"tel:0902040990\"><i class=\"fas fa-phone-alt top-right-1-4\"></i></a></li>\n" +
+        "                        <li><a href=\"https://www.pinterest.com/tokyogateaux/_created/\"><i class=\"fab fa-pinterest top-right-1-5\"></i></a></li>\n" +
+        "                    </ul>\n" +
+        "                </div>\n" +
         "            </div>\n" +
-        "            <div class=\"floatright\">\n" +
-        "                <div class=\"floatright vuong tron fb mautrang dopin\"><a class=\"linkkomau\" href=\"https://www.pinterest.com/tokyogateaux/\"><i class=\" fab fa-pinterest\"></i></a></div>\n" +
-        "                <div class=\"floatright vuong tron fb mautrang xanhdt\"><a class=\"linkkomau\" href=\"tel:0902040990\"><i class=\" fas fa-phone-alt\"></i></a></div>\n" +
-        "                <div class=\"floatright vuong tron fb mautrang denmail\"><a class=\"linkkomau\" href=\"mailto:tokyogateaux@gmail.com\"><i class=\" far fa-envelope\"></i></a></div>\n" +
-        "                <div class=\"floatright vuong tron fb mautrang xanhins\"><a class=\"linkkomau\" href=\"https://www.instagram.com/tokyo.gateaux/\"><i class=\" fab fa-instagram\"></i></a></div>\n" +
-        "                <div class=\"floatright vuong mautrang xanhfb tron fb\"><a class=\"linkkomau\" href=\"https://www.facebook.com/tiembanhsinhnhattokyogateaux/\"><i class=\" fab fa-facebook-f\"></i></a></div>\n" +
-        "            </div>\n" +
-        "        </div>\n" +
-        "    </div>\n" +
+        "        </div>    </div>" +
         "    <div class=\"nentrang\">\n" +
         "        <div class=\"container\">\n" +
         "            <div class=\"top-left-2\">\n" +
